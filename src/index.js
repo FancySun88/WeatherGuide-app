7bf4034d9d2c58c1ceb33b94ab52a04f5e0253d9
FancySun88 <oookkq@gmail.com>
2022-07-03 16:20:25 +0200

    JS Added current date

--- src/index.js
+++ src/index.js
@@ -12,7 +12,59 @@ function showWeather(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
+function formatDate() {
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let time = `${hours}:${minutes}`;
+  return `${time}`;
+}
+
 let apiKey = "6b45fead1f572a2847620f61855bb862";
 let city = "Kyiv";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(showWeather);
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = document.querySelector("#day");
+day.innerHTML = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let date = document.querySelector("#date");
+date.innerHTML = now.getDate();
+
+let month = document.querySelector("#month");
+month.innerHTML = months[now.getMonth()];
+
+let year = document.querySelector("#year");
+year.innerHTML = now.getFullYear();
+
+let time = document.querySelector("#time");
+time.innerHTML = formatDate();
