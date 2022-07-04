97d03b4ea8ce9c95b4c68f0de2f41ed280bb4726
FancySun88 <oookkq@gmail.com>
2022-07-04 20:01:58 +0200

    JS Added weather search for defaul cities, current position

--- src/index.js
+++ src/index.js
@@ -34,7 +34,6 @@ function formatDate() {
 function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#users-input");
-  console.log(cityInputElement.value);
   search(cityInputElement.value);
 }
 
@@ -92,3 +91,51 @@ let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 search("Kyiv");
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(getCurrentPositionWeather);
+}
+
+function getCurrentPositionWeather(position) {
+  let apiKey = "6b45fead1f572a2847620f61855bb862";
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+let currentPosisitionButton = document.querySelector("#button-geoposition");
+currentPosisitionButton.addEventListener("click", getCurrentLocation);
+
+let cityOne = document.querySelector("#defaultCityOne");
+cityOne.addEventListener("click", searchCityOneWeather);
+
+function searchCityOneWeather(event) {
+  event.preventDefault();
+  search("Kyiv");
+}
+
+let cityTwo = document.querySelector("#defaultCityTwo");
+cityTwo.addEventListener("click", searchCityTwoWeather);
+
+function searchCityTwoWeather(event) {
+  event.preventDefault();
+  search("Amsterdam");
+}
+
+let cityThree = document.querySelector("#defaultCityThree");
+cityThree.addEventListener("click", searchCityThreeWeather);
+
+function searchCityThreeWeather(event) {
+  event.preventDefault();
+  search("Valletta");
+}
+
+let cityFour = document.querySelector("#defaultCityFour");
+cityFour.addEventListener("click", searchCityFourWeather);
+
+function searchCityFourWeather(event) {
+  event.preventDefault();
+  search("Poznan");
+}
